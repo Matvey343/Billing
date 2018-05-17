c432054d2756fc4433755b4627565ddcff8612ca
Matthey <matvey343@gmail.com>
2018-05-17 22:50:27 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+
+def main():
+    tariffs = {
+        'basic': {
+            'packages': {},
+            'call': {},
+            'sms': {},
+            'service': {}
+        },
+
+        'normal': {
+            'packages': {},
+            'call': {},
+            'sms': {},
+            'service': {}
+        },
+
+        'business': {
+            'packages': {},
+            'call': {},
+            'sms': {},
+            'service': {}
+        }
+    }
+
+
+if __name__ == '__main__':
+    main()
